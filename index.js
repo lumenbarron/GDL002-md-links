94cc3c796b17c1a8f75650814484c012fd25df95
lumenbarron <lucemendez@gmail.com>
2019-04-15 16:51:11 -0500

    adding test for first function

--- index.js
+++ index.js
@@ -1,33 +1,24 @@
-// module.exports = () => {
-//   // ...
-// };
-
 const fs = require('fs');
 const path = require('path');
-const markdownIt = require('markdown-it');
-const md = new markdownIt ({
-  html: true ,
-  linkify :true
-})
-const folderPath = 'C:\\Users\\LucyM\\Documents\\laboratoria\\GDL002-md-links\\index.js'
+//const markdownIt = require('markdown-it');
+// const md = new markdownIt ({
+//   html: true ,
+//   linkify :true
+// });
+//const folderPath = 'C:\\Users\\LucyM\\Documents\\laboratoria\\GDL002-md-links\\index.js';
 
-// const mdLinks = (path, options) => {
-
-// }
-
-
-const md = (pathFile) => {
+const mdFile = (pathFile) => {
   const pathExt = path.extname(pathFile);
   if (pathExt != '.md') {
     console.log('it doesn´t a file markdown');
-    return false
+    return false;
   } else {
     console.log('it´s a markdown file');
     return true;
   }
 
-}
-console.log(md('readme.md'));
+};
+console.log(mdFile('readme.md'));
 
 
 // fs.readdir(process.argv[2], function (err, files){
@@ -44,11 +35,17 @@ fs.readFile(pathName, function (err, data) {
   if (err) {
       console.log(err);
   } console.log(data.toString());
-})
-}
+});
+};
 
-console.log(readFile('./README.md'));
+//console.log(readFile('./README.md'));
 
-const convertMdtoHtml = (readedFile) => {
-  md.render(readedFile)
-}
+// const convertMdtoHtml = (readedFile) => {
+//   md.render(readedFile)
+// }
+
+module.exports = {
+  mdFile,
+  readFile,
+  
+};
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,12 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
-
+/* Test for know if there is a mdFile*/
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+  it('should be a function', () => {
+    expect(typeof (mdLinks.mdFile)).toBe('function');
   });
-
+  it('should return a file with extension .md when user type', () => {
+    expect(typeof mdLinks.mdFile('readme.md')).toEqual(true);
+  });
 });
+
